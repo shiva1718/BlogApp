7bc0e680938b96c809d50c07644f86fcb11a4dcb
shiva1718 <ms.shiva2019@gmail.com>
2023-08-20 23:02:47 +0530

    added initial post request for blogs

--- backend/index.js
+++ backend/index.js
@@ -14,10 +14,25 @@ async function addUser(userJSON) {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
-        // Send a ping to confirm a successful connection
         await client.db("BlogDB").collection("users").insertOne(userJSON);
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        console.log("Successfully added user to database");
         return true;
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+
+async function addBlog(blog) {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        await client.db("BlogDB").collection("blogs").insertOne(blog);
+        console.log("Successfully added blog to database");
+        return true;
+    } catch (e) {
+        console.log(e);
+        return false;
     } finally {
         // Ensures that the client will close when you finish/error
         await client.close();
@@ -41,14 +56,20 @@ app.get("/", (req, resp) => {
 });
 
 app.post("/register", async (req, resp) => {
-    try {
-        // const user = new User(req.body);
-        // let result = await user.save();
-        addUser(req.body).catch(console.dir);
-        resp.send("User Registered");
-    } catch (e) {
-        resp.send("Something Went Wrong");
-    }
+    let res = addUser(req.body).catch(console.dir);
+    res.then((result) => {
+        resp.status(200).send("User Registered");
+    }).catch((e) => {
+        resp.status(500).send("Something Went Wrong");
+    });
+});
+
+app.post("/addBlog", async (req, resp) => {
+    addBlog(req.body).catch(console.dir).then((result) => {
+        resp.status(200).send("Blog Added");
+    }).catch((e) => {
+        resp.status(500).send("Something Went Wrong");
+    });
 });
 app.listen(5000);
 
